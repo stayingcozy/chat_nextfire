3664ad9e0d75d7f604db5070897d6bdb04893ab9
stayingcozy <cody.colclough@gmail.com>
2023-02-26 01:42:43 -0500

    APP section

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import Navbar from '@/components/Navbar';
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  );
 }
+
---
+++ pages/admin/index.js
@@ -0,0 +1,7 @@
+export default function AdminPostsPage({ }) {
+    return(
+        <main>
+            <h1>Admin posts</h1>
+        </main>
+    )
+}
